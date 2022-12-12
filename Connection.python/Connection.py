ea86adf76a3c83b862e58cfa23fce89d76129865
Santbszc <115561151+Santbszc@users.noreply.github.com>
2022-12-12 10:08:32 -0400

    Update Connection.py

--- Connection.python/Connection.py
+++ Connection.python/Connection.py
@@ -39,6 +39,7 @@ plc = c.Client()
 plc.connect('10.32.22.85',0,2)
 while(True):
     os.system('cls')
+    #Processo da Cabeça de Lavagem
     ar = ReadMemory(plc,27,6,S7WLBit,Areas.MK,0)
     agua = ReadMemory(plc,27,1,S7WLBit,Areas.MK,0)
     dreno = ReadMemory(plc,27,0,S7WLBit,Areas.MK,0)
@@ -46,6 +47,7 @@ while(True):
     retSoda = ReadMemory(plc,27,3,S7WLBit,Areas.MK,0)
     acido = ReadMemory(plc,27,4,S7WLBit,Areas.MK,0)
     retAcido = ReadMemory(plc,27,5,S7WLBit,Areas.MK,0)
+    #Processo da Cabeça de Enchimento
     agua2 =  ReadMemory(plc,28,0, S7WLBit,Areas.MK,0)
     alivioVapor = ReadMemory(plc,28,1, S7WLBit,Areas.MK,0)
     vapor2 = ReadMemory(plc,28,2,S7WLBit,Areas.MK,0)
@@ -53,6 +55,7 @@ while(True):
     ar2 =  ReadMemory(plc,28,4,S7WLBit,Areas.MK,0)
     entradaProduto= ReadMemory(plc,28,5,S7WLBit,Areas.MK,0)
     entradaCerveja = ReadMemory(plc,28,6,S7WLBit,Areas.MK,0)
+    #........................................................
     now = datetime.now()
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
         
@@ -64,6 +67,7 @@ while(True):
         #j = ReadMemory(plc,0,0,S7WLBit,Areas.PA,0)
         # Ler DB (DB)
         #k = ReadMemory(plc,0,0,S7WLBit,Areas.DB,120)
+        
     print("Processo de Lavagem Barril - CHOPP")
     print("Data e Hora", date_time)
     print("\nAr Comprimido - (M27.6):", ar, 
@@ -87,10 +91,3 @@ while(True):
               "\nEntrada Produto - (M28.5):", entradaProduto, 
               "\nEntrada Cerveja - (M28.6):",entradaCerveja) 
     time.sleep(1)
-                
-        #print("Liga(DB120.DBX0.0):", k, 
-        # "\nDesliga(DB120.DBX0.1):", l, 
-        # "\nMotor(DB120.DBX0.2):", m, 
-        # "\nContador Ligado(DB120.DBW8):", n, 
-        # "\nContador Desligado(DB120.DBW10):", o)
-        #time.sleep(0.0)
