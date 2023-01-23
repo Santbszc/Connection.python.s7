f278c500845936f1dfa07ff810e5d2d84340b0e9
Santbszc <115561151+Santbszc@users.noreply.github.com>
2023-01-23 10:44:13 -0400

    att

--- Connection.python/Connection.py
+++ Connection.python/Connection.py
@@ -4,22 +4,20 @@ from snap7.types import *
 import time
 from datetime import datetime
 import os
+import pyodbc
 
-def ReadMemory(plc,byte,bit,datatype,area1,comeco):
-    result = plc.read_area(area1,comeco,byte,datatype)
-    if datatype==S7WLBit:
-        return get_bool(result,0,bit)
-    elif datatype==S7WLByte:
-        return get_sint(result,0)
-    elif datatype==S7WLWord:
-        return get_int(result,0)
-    elif datatype==S7WLReal:
-        return get_real(result,0)
-    elif datatype==S7WLDWord:
-        return get_dint(result,0)
-    else:
-        return None
-    
+#dados_conexão = ("Driver={SQL SERVER};"
+#                 "Server=maquina alocada;"
+#                 "Database=Nome do Banco;")
+#conexao = pyodbc.connect(dados_conexão)
+
+#Conexão ao banco de dados
+dados_conexão = ("Driver = {SQl Server};"
+                 "Server = SANTBS;"
+                 "Database = monitoramentoPreditivo")
+conexao = pyodbc.connect(dados_conexão)
+print("Conexão DB Realizada com sucesso!")
+
 # def ReadMemory2(plc,byte,bit,datatype,area1,comeco):
 #     result = plc.read_area(area1,comeco,byte,datatype)
 #     if datatype==S7WLBit:
@@ -35,10 +33,55 @@ def ReadMemory(plc,byte,bit,datatype,area1,comeco):
 #     else:
 #          return None
 
+#Leitura de memoria PLC
+def ReadMemory(plc,byte,bit,datatype,area1,comeco):
+    result = plc.read_area(area1,comeco,byte,datatype)
+    if datatype==S7WLBit:
+        return get_bool(result,0,bit)
+    elif datatype==S7WLByte:
+        return get_sint(result,0)
+    elif datatype==S7WLWord:
+        return get_int(result,0)
+    elif datatype==S7WLReal:
+        return get_real(result,0)
+    elif datatype==S7WLDWord:
+        return get_dint(result,0)
+    else:
+        return None
+    
+#Escrita de memoria no PLC  (Caso necessário)
+#def writeMemory(plc,byte,bit,datatype,value):
+#    result = plc.read_area(Areas.MK,0,byte, datatype)
+#    if datatype==S7WLBit:
+#        set_bool(result,0,bit,value)
+#    elif datatype==S7WLByte or datatype==S7WLWord:
+#        set_int(result,0,value)
+#    elif datatype==S7WLReal: 
+#        set_real(result,0,value)
+#    elif datatype==S7WLDWord:
+#        set_dword(result,0,value)
+#    plc.write_area(Areas.MK,0,byte,result)
+
+#plc = c.Client()
+#plc.connect('ip da maquina',rack,slot)
+
+# Conexão com o PLC 
 plc = c.Client()
 plc.connect('10.32.22.85',0,2)
+
+#Looping p/acompanhamento das variaveis e falhas
 while True:
     os.system('cls')
+    
+# Ler Memorias (MK)
+#c = ReadMemory(plc,0,2,S7WLBit,Areas.MK,0)
+# Ler Inputs (PE)
+#h = ReadMemory(plc,0,0,S7WLBit,Areas.PE,0)
+# Ler Outputs (PA)
+#j = ReadMemory(plc,0,0,S7WLBit,Areas.PA,0)
+# Ler DB (DB)
+#k = ReadMemory(plc,0,0,S7WLBit,Areas.DB,120)
+
     #Processo da Cabeça de Lavagem
     ar = ReadMemory(plc,27,6,S7WLBit,Areas.MK,0)
     agua = ReadMemory(plc,27,1,S7WLBit,Areas.MK,0)
@@ -47,6 +90,7 @@ while True:
     retSoda = ReadMemory(plc,27,3,S7WLBit,Areas.MK,0)
     acido = ReadMemory(plc,27,4,S7WLBit,Areas.MK,0)
     retAcido = ReadMemory(plc,27,5,S7WLBit,Areas.MK,0)
+    
     #Processo da Cabeça de Enchimento
     agua2 =  ReadMemory(plc,28,0, S7WLBit,Areas.MK,0)
     alivioVapor = ReadMemory(plc,28,1, S7WLBit,Areas.MK,0)
@@ -55,24 +99,18 @@ while True:
     ar2 =  ReadMemory(plc,28,4,S7WLBit,Areas.MK,0)
     entradaProduto= ReadMemory(plc,28,5,S7WLBit,Areas.MK,0)
     entradaCerveja = ReadMemory(plc,28,6,S7WLBit,Areas.MK,0)
+    
     #Junção das memórias para variaveis
     Var = [ar,agua,dreno,soda,retSoda,acido,retAcido,agua2,alivioVapor,vapor2,entradaCO2,ar2,entradaProduto,entradaCerveja]
+    
     #envio das Variaveis para TXT (Manipulação do dash)
     f = open('dashBoard.txt', 'w')
     f.write(str(Var))
-    #........................................................
+    
     now = datetime.now()
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-        
-        # Ler Memorias (MK)
-        #c = ReadMemory(plc,0,2,S7WLBit,Areas.MK,0)
-        # Ler Inputs (PE)
-        #h = ReadMemory(plc,0,0,S7WLBit,Areas.PE,0)
-        # Ler Outputs (PA)
-        #j = ReadMemory(plc,0,0,S7WLBit,Areas.PA,0)
-        # Ler DB (DB)
-        #k = ReadMemory(plc,0,0,S7WLBit,Areas.DB,120)
-        
+    
+    #Retorno ao terminal das variaveis para o monitoramento           
     print("Processo de Lavagem Barril - CHOPP")
     print("Data e Hora", date_time)
     print("\nAr Comprimido - (M27.6):", ar, 
